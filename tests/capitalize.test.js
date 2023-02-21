ea2818c5843ff23855c6ee5bb959feb3e161f02c
gendzmin <gendzmin@gmail.com>
2023-02-21 18:43:28 +0300

    starting git

---
+++ tests/capitalize.test.js
@@ -0,0 +1,6 @@
+import { strict as assert } from 'node:assert';
+import capitalize from "../src/capitalize.js";
+
+assert.equal(capitalize('hello'), 'Hello');
+assert.equal(capitalize(''), '');
+console.log('Тесты пройдены!');
